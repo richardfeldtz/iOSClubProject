d7a4c1e7aecf359954ebb2d6195477e42d5fb40a
Richard Feldtz <rcfeldtz@icloud.com>
2016-02-24 19:23:32 -0500

    sounds

--- iOSClubProject/LeftRightViewController.m
+++ iOSClubProject/LeftRightViewController.m
@@ -7,12 +7,14 @@
 //
 
 #import "LeftRightViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface LeftRightViewController ()
 
 @property (nonatomic, weak) IBOutlet UIImageView *leftHandImageView;
 @property (nonatomic, weak) IBOutlet UIImageView *rightHandImageView;
 @property (nonatomic, assign) BOOL isGuess;
+@property  (nonatomic, strong) AVAudioPlayer *player;
 
 @end
 
@@ -21,6 +23,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    NSURL *soundURL = [[NSBundle mainBundle] URLForResource:@"ding" withExtension:@"mp3"];
+    self.player = [[AVAudioPlayer alloc] initWithContentsOfURL:soundURL error:nil];
 }
 
 -(IBAction)leftButtonPressed {
@@ -40,9 +45,15 @@
     NSInteger side = arc4random() % 2;
     if (side == 0) {
         self.leftHandImageView.image = [UIImage imageNamed:@"hand"];
+        if (didGuessWithLeft) {
+            [self playerGuessedCorrectly];
+        }
     }
     else if (side == 1) {
         self.rightHandImageView.image = [UIImage imageNamed:@"hand"];
+        if (!didGuessWithLeft) {
+            [self playerGuessedCorrectly];
+        }
     }
     
     [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(resetHands) userInfo:nil repeats:NO];
@@ -52,6 +63,10 @@
     self.isGuess = NO;
     self.leftHandImageView.image = [UIImage imageNamed:@"Fist"];
     self.rightHandImageView.image = [UIImage imageNamed:@"Fist"];
+}
+
+-(void)playerGuessedCorrectly {
+    [self.player play];
 }
 
 
